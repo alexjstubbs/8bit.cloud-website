4a16cf4ad824ef9b3c431dc6e15e5db837888be1
alex <alex@alexstubbs.com>
2016-01-24 06:02:57 -0500

    Slack and platform additions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
  * Ignition.io website. Copyright 2016 Alexander Stubbs. All Rights Reserved.
  * Creative Commons Attribution NonCommercial (CC-BY-NC) 
@@ -7,29 +9,32 @@
  *
  */
 
-var moment = require('moment/moment.js'),   
-    $      = require('jquery/dist/jquery.js');
+var $      = require('jquery'),
+    bs     = require('bootstrap'),
+    tips   = require('../js/tips'),
+    moment = require('moment/moment.js');
+   
 
 function updateHeader(sum) {
     $("#lines").text(sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 }
 
 $(document).ready(function($) {
- 
+
     var fStats = $("#fStat").text(),
         fStats = fStats.mtime,
         fStats = moment(fStats).fromNow();
 
         $("#commit").text(fStats);
 
-        fetch('https://api.github.com/repos/alexjstubbs/ignition-dev/stats/contributors');
-        .then((response)            => response.json());
-        .then((contributors)        => contributors;
-        .map((contributor)          => contributor.weeks;
-        .reduce((lineCount, week)   => lineCount + week.a - week.d, 0)));
-        .then((lineCounts)          => lineCounts.reduce((lineTotal, lineCount) => lineTotal + lineCount));
+        fetch('https://api.github.com/repos/alexjstubbs/ignition-dev/stats/contributors')
+        .then((response)            => response.json())
+        .then((contributors)        => contributors
+        .map((contributor)          => contributor.weeks
+        .reduce((lineCount, week)   => lineCount + week.a - week.d, 0)))
+        .then((lineCounts)          => lineCounts.reduce((lineTotal, lineCount) => lineTotal + lineCount))
         .then((lines)               => updateHeader(lines));
-        
 
+        
 
 })
